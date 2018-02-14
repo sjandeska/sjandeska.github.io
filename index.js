79bdf4d759a29d486cefd163a40da1667f262ee9
sjandeska <sara.jandeska@kellogg.northwestern.edu>
2018-02-14 16:07:10 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+
+ //
+// Your code goes here:
+
+let movietime =function(){
+    // console.log("is this working?")
+    let w =[0,1,2,3,4,5,6,7,8]
+  let smallImage= jQuery("#thumbnails img[w]")
+  jQuery("#main-image").attr("smallImage")}
+      //   let theImage=jQuery(this)
+      // console.log("sdfsdf:", theImage)
+      // console.log(theImage.attr("src"))
+  // jQuery("#thumbnails img[w]").attr("height": "450", "width": "300")
+
+
+// //
+//   jQuery("#thumbnails img").click(function(){});
+//   jQuery("#main-img").attr("src", " https://image.tmdb.org/t/p/w300/q0R4crx2SehcEEQEkYObktdeFy.jpg");
+// // let time=function(){
+//
+// let movietime= function (){
+//   Movie=jQuery("#main-image").attr("width", "300").attr("height", "450");
+//   console.log (Movie)
+//
+// let Movie=jQuery("#thumbnails img")
+//
+// let movieOne = Movie[0]
+// let movieTwo = Movie[1]
+// let movieThree =Movie [2]
+// let movieFour = Movie [3]
+// let movieFive = Movie [4]
+// let movieSix = Movie [5]
+// let movieSeven = Movie [6]
+// let movieEight = Movie [7]
+//
+// let firstMovie = jQuery('#thumbnails img')[0]
+// let secondMovie = jQuery('#thumbnails img')[1]
+// let thirdMovie = jQuery('#thumbnails img')[2]
+// let fourthMovie = jQuery('#thumbnails img')[3]
+// let fifthMovie = jQuery('#thumbnails img')[4]
+// let sixthMovie = jQuery('#thumbnails img')[5]
+// let seventhMovie = jQuery('#thumbnails img')[6]
+// let eighthMovie = jQuery('#thumbnails img')[7]
+// }
+
+//   let movies= [https://image.tmdb.org/t/p/w300/44sKJOGP3fTm4QXBcIuqu0RkdP7.jpg",
+//   https://image.tmdb.org/t/p/w300/q0R4crx2SehcEEQEkYObktdeFy.jpg",
+//   https://image.tmdb.org/t/p/w300/h6O5OE3ueRVdCc7V7cwTiQocI7D.jpg",
+//   https://image.tmdb.org/t/p/w300/dfhztJRiElqmYW4kpvjYe1gENsD.jpg",
+//   https://image.tmdb.org/t/p/w300/5aGhaIHYuQbqlHWvWYqMCnj40y2.jpg",
+// //   https://image.tmdb.org/t/p/w300/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg"];
+// // // jQuery("movies"[1]).on("click", movietime)
+let x =[0,1,2,3,4,5,6,7,8];
+ jQuery("#thumbnails img[x]").on("click", movietime)
